3836b31573c4961de7191717a968823d3952e91c
natasablagojevic <blagojevicnatasa360@gmail.com>
2022-11-12 13:53:28 +0100

    ojler

---
+++ grafovi/ojler.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<stack>
+#include<limits>
+
+using namespace std;
+
+#define INF numeric_limits<int>::max()
+
+struct Graf
+{
+    int V;
+    vector<vector<int>> lista_susedstva;
+    vector<bool> posecen; 
+    vector<int> stepeni;
+};
+
+void inicjalizuj_graf(Graf &g, int V)
+{
+    g.V = V;
+    g.lista_susedstva.resize(V);
+    g.posecen.resize(V, false);
+    g.stepeni.resize(V, 0);
+}
+
+void dodaj_granu(Graf &g, int u, int v)
+{
+    g.lista_susedstva[u].push_back(v);
+    g.lista_susedstva[v].push_back(u);
+    g.stepeni[u]++;
+    g.stepeni[v]++;
+}
+
+int broj_cvorova_neparnog_stepena(Graf &g)
+{
+    int broajc = 0;
+    for (int i = 0; i < g.V; i++)
+        if (g.stepeni[i] % 2 != 0)
+            broajc++;
+
+    return broajc;
+}
+
+void DFS(Graf &g, int u)
+{
+    g.posecen[u] = true;
+
+    auto begin = g.lista_susedstva[u].begin();
+    auto end = g.lista_susedstva[u].end();
+
+    while (begin != end) {
+        if (!g.posecen[*begin])
+            DFS(g, *begin);
+
+        begin++;
+    }
+}
+
+bool je_povezan(Graf &g)
+{
+    int start;
+    for (int i = 0; i < g.V; i++)
+        if (g.lista_susedstva[i].size() > 0)
+            start = i;
+
+    DFS(g, start);
+
+    for (int i = 0; i < g.V; i++)
+        if (!g.posecen[i] && g.stepeni[i] > 0)
+            return false;
+
+    return true;
+}
+
+/* 
+    0 -> nije
+    1 -> ciklus
+    2 -> put
+ */
+int ojler(Graf &g)
+{
+    int br_nep = broj_cvorova_neparnog_stepena(g);
+    if (br_nep > 2)
+        return 0;
+
+    if (!je_povezan(g))
+        return 0;
+
+    return br_nep == 0 ? 2 : 1;
+}
+
+int main()
+{
+
+
+
+    return 0;
+}
