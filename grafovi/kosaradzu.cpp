eee9986f8cd82622d1ddc11efe591c79fa60f781
natasablagojevic <blagojevicnatasa360@gmail.com>
2022-11-12 13:37:40 +0100

    KOSARADZU - komponente jake povezanosti

---
+++ grafovi/kosaradzu.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<stack>
+#include<limits>
+
+using namespace std;
+
+#define INF numeric_limits<int>::max()
+
+struct Graf
+{
+    int V;
+    vector<vector<int>> lista_susedstva;
+    vector<bool> posecen; 
+};
+
+void inicjalizuj_graf(Graf &g, int V)
+{
+    g.V = V;
+    g.lista_susedstva.resize(V);
+    g.posecen.resize(V, false);
+}
+
+void dodaj_granu(Graf &g, int u, int v)
+{
+    g.lista_susedstva[u].push_back(v);
+}
+
+int main()
+{
+
+
+
+    return 0;
+}
