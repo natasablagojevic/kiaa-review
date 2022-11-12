fb139de2438ef7b0dd80154e3286390fce9aefab
natasablagojevic <blagojevicnatasa360@gmail.com>
2022-11-12 13:43:34 +0100

    pr1

--- grafovi/kosaradzu.cpp
+++ grafovi/kosaradzu.cpp
@@ -1,3 +1,4 @@
+/* Ovo je algoritam koji samo indentifikuje da li postoje komponente jake povezanosti, ali ih ne ispisuje */
 #include<iostream>
 #include<vector>
 #include<queue>
@@ -28,9 +29,71 @@ void dodaj_granu(Graf &g, int u, int v)
     g.lista_susedstva[u].push_back(v);
 }
 
+Graf obrni_grane(Graf &g)
+{
+    Graf pom;
+    inicjalizuj_graf(pom, g.V);
+
+    for (int i = 0; i < g.V; i++)
+        for (int u : g.lista_susedstva[i])
+            dodaj_granu(pom, u, i);
+
+    return pom;
+}
+
+void DFS(Graf &g, int u)
+{
+    g.posecen[u] = true;
+
+    auto begin = g.lista_susedstva[u].begin();
+    auto end = g.lista_susedstva[u].end();
+
+    while (begin != end) {
+        if (!g.posecen[*begin])
+            DFS(g, *begin);
+
+        begin++;
+    }
+}
+
+int broj_neposecenih(Graf &g)
+{
+    int brojac = 0;
+    for (int i = 0; i < g.V; i++)
+        if (!g.posecen[i])
+            brojac++;
+
+    return brojac;
+}
+
+bool kosaradzu(Graf &g, int u)
+{
+    DFS(g, u);
+    if (broj_neposecenih(g) > 0)
+        return false;
+
+    Graf pom = obrni_grane(g);
+    DFS(pom, u);
+
+    if (broj_neposecenih(pom) > 0)
+        return false;
+
+    return true;
+}
+
 int main()
 {
+    Graf g;
 
+    inicjalizuj_graf(g, 5);
+    dodaj_granu(g, 0, 1);
+    dodaj_granu(g, 1, 2);
+    dodaj_granu(g, 2, 3);
+    dodaj_granu(g, 2, 4);
+    dodaj_granu(g, 3, 0);
+    dodaj_granu(g, 4, 2);
+
+    cout << boolalpha << kosaradzu(g, 0) << endl;
 
 
     return 0;
