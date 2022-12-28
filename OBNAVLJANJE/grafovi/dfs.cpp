08abe53d863953de54905b4e3b1b17318ff4b374
natasablagojevic <blagojevicnatasa360@gmail.com>
2022-12-28 16:11:50 +0100

    DFS

---
+++ OBNAVLJANJE/grafovi/dfs.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<algorithm>
+#include<queue>
+#include<cmath>
+#include<numeric>
+#include<limits>
+
+#define INF numeric_limits<int>::max()
+
+using namespace std;
+
+struct Graf
+{
+    int V;
+    vector<vector<int>> lista_susedstva;
+    vector<bool> posecen;
+};
+
+void inicijalizuj_graf(Graf &g, int V)
+{
+    g.V = V;
+    g.lista_susedstva.resize(V);
+    g.posecen.resize(V, false);
+}
+
+void dodaj_granu(Graf &g, int u, int v)
+{
+    g.lista_susedstva[u].push_back(v);
+}
+
+void DFS(Graf &g, int u)
+{
+    g.posecen[u] = true;
+
+    cout << u << ' ';
+
+    auto begin = g.lista_susedstva[u].begin();
+    auto end = g.lista_susedstva[u].end();
+
+    while (begin != end) {
+        if (!g.posecen[*begin])
+            DFS(g, *begin);
+
+        begin++;
+    }
+}
+
+int main()
+{
+    Graf g;
+
+    inicijalizuj_graf(g, 4);
+    dodaj_granu(g, 0, 1);
+    dodaj_granu(g, 0, 2);
+    dodaj_granu(g, 1, 2);
+    dodaj_granu(g, 2, 0);
+    dodaj_granu(g, 2, 3);
+    dodaj_granu(g, 3, 3);
+
+    DFS(g, 0);
+
+    return 0;
+}
