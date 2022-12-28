db7f9acd8ca84b2b1e725107e359f1440662314e
natasablagojevic <blagojevicnatasa360@gmail.com>
2022-12-28 16:18:56 +0100

    ...

---
+++ Grafovi/kostur.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<set>
+#include<numeric>
+#include<limits>
+
+#define INF numeric_limits<int>::max()
+
+using namespace std;
+
+struct Graf
+{
+    int V;
+    vector<vector<int>> lista_susedstva;
+    vector<bool> posecen;
+};
+
+void inicijalizuj_graf(Graf &g, int V)
+{
+    g.V = V;
+    g.lista_susedstva.resize(V);
+    g.posecen.resize(V, false);
+}
+
+void dodaj_granu(Graf &g, int u, int v)
+{
+    g.lista_susedstva[u].push_back(v);
+}
+
+int main()
+{
+
+
+    return 0;
+}
---
+++ OBNAVLJANJE/geometrija/kostur.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+struct Tacka
+{
+    int x;
+    int y;
+};
+
+int kvadratno_rastojanje(Tacka a, Tacka b)
+{
+    return (a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y); 
+}
+
+int determinanta(Tacka a, Tacka b)
+{
+    return a.x*b.y - a.y*b.x;
+}
+
+int orjentacija(Tacka A, Tacka B, Tacka C)
+{
+    Tacka AB;
+    AB.x = B.x - A.x, AB.y = B.y - A.y;
+    Tacka AC;
+    AC.x = C.x - A.x, AC.y = C.y - A.y;
+
+    int result = determinanta(AB, AC);
+
+    return result == 0 ? 0 : (result > 0 ? 1 : -1);
+}
+
+bool sa_iste_strane_prave(Tacka A, Tacka B, Tacka C, Tacka D)
+{
+    int ABC = orjentacija(A, B, C);
+    int ABD = orjentacija(A, B, D);
+
+    return ABC == ABD;
+}
+
+bool u_trouglu(Tacka A, Tacka B, Tacka C, Tacka M)
+{
+    int ABM = orjentacija(A, B, M);
+    int BCM = orjentacija(B, C, M);
+    int CAM = orjentacija(C, A, M);
+
+    return ABM == BCM && ABM == CAM && BCM == CAM;
+}
+
+int main()
+{
+
+
+    return 0;
+}
