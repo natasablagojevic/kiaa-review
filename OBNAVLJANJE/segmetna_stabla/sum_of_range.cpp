62c94b0539a1dda812c0e222e88d90bb571047bc
natasablagojevic <blagojevicnatasa360@gmail.com>
2022-12-28 16:17:46 +0100

    sum of range

---
+++ OBNAVLJANJE/segmetna_stabla/sum_of_range.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+
+void build(vector<int> &a, vector<int> &tree, int k, int x, int y)
+{
+    if (x == y) {
+        tree[k] = a[x];
+        return ;
+    }
+
+    int s = (x+y)/2;
+
+    build(a, tree, 2*k, x, s);
+    build(a, tree, 2*k + 1, s + 1, y);
+
+    tree[k] = tree[2*k] + tree[2*k + 1];
+}
+
+int sum_of_range(vector<int> &tree, int k, int x, int y, int a, int b)
+{
+    if (x > b || y < a)
+        return 0;
+
+    if (x >= a && y <= b)
+        return tree[k];
+
+    int s = (x+y)/2;
+
+    return sum_of_range(tree, 2*k, x, s, a, b) + sum_of_range(tree, 2*k + 1, s + 1, y, a, b);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int h = (int)ceil(log2(n));
+    int N = pow(2, h+1);
+
+    vector<int> tree(N);
+    build(a, tree, 1, 0, n-1);
+
+    int x, y;
+    cout << "Segment: " << endl;
+    cin >> x >> y;
+
+    cout << sum_of_range(tree, 1, 0, n-1, x, y) << endl;
+
+    // for (int x : tree)
+    //     cout << x << ' ';
+    // cout << endl;
+
+    return 0;
+}
