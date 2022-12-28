65b128cdb3ad5fd5f33157cd019b0d11752d683f
natasablagojevic <blagojevicnatasa360@gmail.com>
2022-12-28 16:15:01 +0100

    DFS

---
+++ OBNAVLJANJE/graphs/dfs.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<numeric>
+#include<queue>
+#include<stack>
+
+using namespace std;
+
+struct Graf
+{
+    int V;
+    vector<vector<int>> lista_susedstva;
+    vector<bool> posecen;
+};
+
+void inicijalizuj_graf(Graf &g, int V)
+{
+    g.V = V;
+    g.lista_susedstva.resize(V);
+    g.posecen.resize(V, false);
+}
+
+void dodaj_granu(Graf &g, int u, int v)
+{
+    g.lista_susedstva[u].push_back(v);
+}
+
+void DFS(Graf &g, int u)
+{
+    g.posecen[u] = true;
+
+    cout << u << ' ';
+
+    auto begin = g.lista_susedstva[u].begin();
+    auto end = g.lista_susedstva[u].end();
+
+    while (begin != end) {
+        if (!g.posecen[*begin])
+            DFS(g, *begin);
+
+        begin++;
+    }
+}
+
+int main()
+{
+
+
+
+    return 0;
+}
