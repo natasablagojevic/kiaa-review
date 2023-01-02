697f1ed7d7858da83d04877d3a903a59e37b748b
natasablagojevic <blagojevicnatasa360@gmail.com>
2023-01-02 15:20:21 +0100

    minimalno povezujuce drvo

---
+++ Grafovi/prim.cpp
@@ -0,0 +1,104 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<set>
+#include<numeric>
+#include<limits>
+
+#define INF numeric_limits<int>::max()
+
+using namespace std;
+
+struct Graf
+{
+    int V;
+    vector<vector<int>> matrica;
+    vector<bool> posecen;
+    vector<bool> MST;
+    vector<int> roditelj;
+    vector<int> grane;
+
+};
+
+void inicijalizuj_graf(Graf &g, int V)
+{
+    g.V = V;
+    g.matrica.resize(V, vector<int> (V));
+    g.posecen.resize(V, false);
+    g.MST.resize(V, false);
+    g.grane.resize(V, INF);
+    g.roditelj.resize(V, -1);
+}
+
+void dodaj_granu(Graf &g, int u, int v, int t)
+{
+    g.matrica[u][v] = g.matrica[v][u] = t;
+}
+
+int min_grana_neukljucena(Graf &g)
+{
+    int min = INF;
+    int min_index = -1;
+
+    for (int i = 0; i < g.V; i++)
+        if (!g.MST[i] && g.grane[i] < min) {
+            min = g.grane[i];
+            min_index = i;
+        }
+
+    return min_index;
+}
+
+void prim(Graf &g, int u)
+{
+    g.grane[u] = 0;
+
+    int cvor;
+
+    for (int i = 0; i < g.V - 1; i++) {
+        cvor = min_grana_neukljucena(g);
+        
+        g.MST[cvor] = true;
+
+        for (int v = 0; v < g.V; v++) {
+            if (!g.MST[v] && g.matrica[cvor][v] && g.grane[v] > g.matrica[cvor][v]) {
+                g.grane[v] = g.matrica[cvor][v];
+                g.roditelj[v] = cvor;
+            }
+        }
+    }
+
+    int cena = 0;
+    for (int i = 0; i < g.V; i++)
+        if (i != u)
+            cena += g.grane[i];
+
+    cout << cena << endl;
+}
+
+void print_prim(Graf &g, int u)
+{
+    for (int i = 1; i < g.V; i++)
+        cout << g.roditelj[i] << " - " << i << "\t" << g.matrica[g.roditelj[i]][i] << endl;
+}
+
+int main()
+{
+
+    Graf g;
+    inicijalizuj_graf(g, 5);
+
+    dodaj_granu(g, 0, 1, 1);
+    dodaj_granu(g, 0, 2, 5);
+    dodaj_granu(g, 0, 3, 10);
+    dodaj_granu(g, 0, 4, 4);
+    dodaj_granu(g, 1, 2, 2);
+    dodaj_granu(g, 1, 4, 1);
+    dodaj_granu(g, 2, 3, 4);
+
+    prim(g, 0);
+    print_prim(g, 0);
+
+    return 0;
+}
