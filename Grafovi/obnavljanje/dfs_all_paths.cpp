3fdb07f45f05604755290621bbb49c75aad9da25
natasablagojevic <blagojevicnatasa360@gmail.com>
2023-01-02 15:21:11 +0100

    DFS sve putanje

---
+++ Grafovi/obnavljanje/dfs_all_paths.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<set>
+#include<numeric>
+#include<limits>
+
+#define INF numeric_limits<int>::max()
+
+struct Graf 
+{
+    int V;
+    std::vector<std::vector<int>> lista_susedstva;
+    std::vector<bool> posecen;
+};
+
+void inicijalizuj_graf(Graf &g, int V)
+{
+    g.V = V;
+    g.lista_susedstva.resize(V);
+    g.posecen.resize(V, false);
+}
+
+void dodaj_granu(Graf &g, int u, int v)
+{
+    g.lista_susedstva[u].push_back(v);
+}
+
+void DFS(Graf &g, int u, int v, std::vector<int> result)
+{
+    if (u == v) {
+        result.push_back(v);
+        int i, n = result.size();
+        for (i = 0; i < n-1; i++)
+            std::cout << result[i] << " -> ";
+        std::cout << result[i] << std::endl;
+        return ;
+    }
+
+    auto begin = g.lista_susedstva[u].begin();
+    auto end = g.lista_susedstva[u].end();
+
+    result.push_back(u);
+
+    while (begin != end) {
+        if (!g.posecen[*begin])
+            DFS(g, *begin, v, result);
+
+        begin++;
+    }
+
+    g.posecen[u] = false;
+}
+
+int main()
+{
+
+
+    return 0;
+}
