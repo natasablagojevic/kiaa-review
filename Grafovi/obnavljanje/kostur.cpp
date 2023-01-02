30d514b636302ce19294ffa7d0aa93cde379901c
natasablagojevic <blagojevicnatasa360@gmail.com>
2023-01-02 15:24:46 +0100

    .

---
+++ Grafovi/obnavljanje/kostur.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<set>
+#include<numeric>
+#include<limits>
+
+#define INF numeric_limits<int>::max()
+
+struct Graf 
+{
+    int V;
+    std::vector<std::vector<int>> lista_susedstva;
+    std::vector<bool> posecen;
+};
+
+void inicijalizuj_graf(Graf &g, int V)
+{
+    g.V = V;
+    g.lista_susedstva.resize(V);
+    g.posecen.resize(V, false);
+}
+
+void dodaj_granu(Graf &g, int u, int v)
+{
+    g.lista_susedstva[u].push_back(v);
+}
+
+int main()
+{
+
+
+    return 0;
+}
